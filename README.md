37a11290aa7e46956c0fe47f31b8b6f05b5d6b44
Max New <maxsnew@gmail.com>
2013-06-13 19:37:11 -0500

    Usage script and instructions.

---
+++ README.md
@@ -0,0 +1,20 @@
+SETUP
+=====
+
+# setup.rb
+Run setup.rb on 2 directories full of traffic files with the desired output directories.
+
+# ds
+Run
+
+`ds -w [odir]/data.ary [odir]/data`
+
+for each of the 2 odirs to do some preproccessing.
+
+RUNNING
+=======
+Run
+
+`siggen -S [user_traffic_odir] -N [noise_traffic_odir] -G [output_dir]`
+
+where output_dir is empty.
---
+++ setup.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+
+if ARGV.length != 2
+  puts 'usage: setup.rb <traffic-dir> <odir>'
+  exit 1
+end
+
+dir_name = ARGV[0]
+out_name = ARGV[1]
+
+unless File.directory? dir_name
+  puts "#{dir_name} is not a directory"
+  exit 1
+end
+
+traffic = Dir.new dir_name
+
+def makeFile(fname)
+  if File.exist? fname
+    File.delete fname
+  end
+  File.new fname, "w"
+end
+puts out_name
+dataFile = makeFile (File.join out_name, "data")
+offFile = makeFile (File.join out_name, "offsets")
+
+# Change data format here.
+def process(inp)
+  inp
+end
+
+# output to 2 files, the offsets and the data
+offsets = traffic.map do |fname|
+  next if fname == '.' or fname == '..'
+  name = File.join traffic, fname
+  f = File.new name, "r"
+  dataFile.write process(f.read)
+  [File.stat(name).size].pack "L<"
+end.select do |x|
+  x != nil
+end.join
+
+offFile.write offsets
+
+# puts offsets.join
+
+# noise_traffic_dir = File.new ARGV[1], "r"
+
